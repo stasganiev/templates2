e85ca6e5a6c145912b7d66cc7f13da30b6beb2ec
Stas Ganiev <stas@ganiev.pro>
2022-11-19 01:06:03 +0100

    Add browser's reloading and consolidation js

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+
+function browsersync() {
+  browserSync.init({ // Initiolized Browsersync
+    server: { baseDir: 'app/' }, // Source files folder
+    notify: false, // Turned off notificasions
+    online: true // Working mode: true or false
+  })
+}
+
+// Concatination and minified js
+function scripts() {
+  return src([ // Source files list
+    'app/js/app.js', // User's script using library
+    ])
+  .pipe(concat('app.min.js')) // Concatination to one file
+  .pipe(uglify()) // Compressed JavaScript
+  .pipe(dest('app/js/')) // Put result to distination folder
+  .pipe(browserSync.stream()) // Reload Browsersync
+}
+
+exports.browsersync = browsersync;
