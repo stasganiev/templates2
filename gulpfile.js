c0a082dbe0a6dd20257433744a8aa6a1045cb3d3
Stas Ganiev <stas@ganiev.pro>
2022-11-20 17:33:09 +0100

    Finished basic gulp build. Added sass, images, cleaning, build destination

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-let preprocessor = 'sass';
 const { src, dest, parallel, series, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
@@ -6,6 +5,8 @@ const uglify = require('gulp-uglify-es').default;
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const cleancss = require('gulp-clean-css');
+const imagecomp = require('compress-images');
+const clean = require('gulp-clean');
 
 function browsersync(done) {
   browserSync.init({ // Initiolized Browsersync
@@ -27,23 +28,62 @@ function scripts() { // Concatination and minified js
   .pipe(browserSync.stream()) // Reload Browsersync
 }
 
-function sass() {
-	return src('app/sass/main.scss')
-	.pipe(eval('sass')())
-	.pipe(concat('app.min.css'))
-	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-	.pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Minimize
-	.pipe(dest('app/css/'))
-	.pipe(browserSync.stream())
+function styles() {
+  return src('app/sass/main.scss')
+  .pipe(eval('sass')())
+  .pipe(concat('app.min.css'))
+  .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+  .pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Minimize
+  .pipe(dest('app/css/'))
+  .pipe(browserSync.stream())
+}
+
+async function images() {
+  imagecomp(
+    "app/img/src/**/*",
+    "app/img/dest/",
+    { compress_force: false, statistic: true, autoupdate: true }, false,
+    { jpg: { engine: "mozjpeg", command: ["-quality", "75"] } },
+    { png: { engine: "pngquant", command: ["--quality=75-100", "-o"] } },
+    { svg: { engine: "svgo", command: "--multipass" } },
+    { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+    function (err, completed) {
+      if (completed === true) {
+        browserSync.reload()
+      }
+    }
+  )
+}
+
+function cleanimg() {
+  return src('app/img/dest/', {allowEmpty: true}).pipe(clean())
+}
+
+function buildcopy() {
+  return src([
+    'app/css/**/*.min.css',
+    'app/js/**/*.min.js',
+    'app/img/dest/**/*',
+    'app/**/*.html',
+    ], { base: 'app' })
+  .pipe(dest('dist'))
+}
+
+function cleandist() {
+  return src('dist', {allowEmpty: true}).pipe(clean()) // Удаляем папку "dist/"
 }
 
 function startwatch() {
-	watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
-  watch('app/**/sass/**/*.scss', sass);
+  watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+  watch('app/**/sass/**/*.scss', styles);
   watch('app/**/*.html').on('change', browserSync.reload);
+  watch('app/img/src/**/*', images);
 }
 
 exports.browsersync = browsersync;
 exports.scripts = scripts;
 exports.styles = styles;
-exports.default = parallel(sass, scripts, browsersync, startwatch);
+exports.images = images;
+exports.cleanimg = cleanimg;
+exports.default = parallel(styles, scripts, browsersync, startwatch);
+exports.build = series(cleandist, styles, scripts, images, buildcopy);
